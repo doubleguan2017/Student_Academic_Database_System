6aa42080ae8995036be91655a1dc1e7e3bd14534
doubleguan2017 <doubleguan0726@gmail.com>
2019-01-16 16:01:34 -0600

    Caesar Student System

---
+++ MYSQL/coursecheck.sql
@@ -0,0 +1,115 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `coursecheck_correctinput`(in username varchar(20), in CourseNum char(20), in eSemester Char(5), in eyear char(20), out notif varchar(150), out output int)
+BEGIN
+
+declare t_error int default 0;
+declare continue handler for sqlexception set t_error=1;
+START transaction;
+/* satisfy pre */
+IF EXISTS(
+SELECT R.UoSCode
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND R.UoSCode = CourseNum AND R.Year = eyear AND R.Semester = eSemester AND R.PrereqUoSCode = T.UoSCode AND (T.Grade = "CR" OR T. Grade = "P" OR T.Grade = "D") AND R.Enrollment < R.MaxEnrollment)
+THEN
+UPDATE uosoffering
+SET Enrollment = Enrollment + 1
+WHERE UoSCode = CourseNum;
+INSERT INTO transcript
+VALUES(username, CourseNum, eSemester, eyear, NULL);
+SET notif = "E";
+SET output = 1;
+
+
+/* satisfy pre but number of enroll*/
+ELSEIF EXISTS(
+SELECT R.UoSCode
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND R.UoSCode = CourseNum AND R.Year = eyear AND R.Semester = eSemester AND R.PrereqUoSCode = T.UoSCode AND (T.Grade = "CR" OR T. Grade = "P" OR T.Grade = "D") AND (R.Enrollment > R.MaxEnrollment OR R.Enrollment = R.MaxEnrollment))
+THEN
+SET notif = "Enroll_failed.The_course_is_beyond_maxenrollment!!!";
+SET output = 0;
+
+/* Not satisfy pre */
+ELSEIF EXISTS
+(
+SELECT R.UoSCode
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND (R.UoSCode = CourseNum AND R.Year = eyear AND R.Semester = eSemester AND R.PrereqUoSCode = T.UoSCode AND (T.Grade is null OR T.Grade = "F"))
+)
+THEN
+select R.PrereqUoSCode into notif
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND (R.UoSCode = CourseNum AND R.PrereqUoSCode = T.UoSCode AND (T.Grade is null OR T.Grade = "F"));
+SET output = 0;
+ELSEIF EXISTS
+(
+select R.UoSCode
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND R.UoSCode = CourseNum AND R.Year = eyear AND R.Semester = eSemester AND R.PrereqUoSCode not in 
+(SELECT T.UoSCode
+FROM transcript T)
+group by R.PrereqUoSCode 
+)
+THEN
+select R.PrereqUoSCode into notif
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND R.UoSCode = CourseNum AND R.PrereqUoSCode not in 
+(SELECT T.UoSCode
+FROM transcript T)
+group by R.PrereqUoSCode;
+SET output = 0;
+
+
+/* No prereq */
+ELSEIF EXISTS
+(SELECT R.UoSCode
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND R.UoSCode <> CourseNum AND CourseNum 
+IN
+(SELECT U.UoSCode
+FROM uosoffering U
+where U.UoSCode = CourseNum and U.Enrollment < U.MaxEnrollment 
+))
+THEN
+UPDATE uosoffering
+SET Enrollment = Enrollment + 1
+WHERE UoSCode = CourseNum;
+INSERT INTO transcript
+VALUES(Username, CourseNum, eSemester, eyear, NULL);
+SET notif = "En";
+SET output = 1;
+/* No prereq but beyond enrollment*/
+ELSEIF EXISTS
+(SELECT R.UoSCode
+from requiredpre_enroll R, transcript T
+where T.StudId = username AND R.UoSCode <> CourseNum AND CourseNum 
+IN
+(SELECT U.UoSCode
+FROM uosoffering U
+where U.UoSCode = CourseNum AND (U.enrollment > U.MaxEnrollment OR U.Enrollment = U.MaxEnrollment)
+))
+THEN
+SET notif = "Enroll_failed.The_course_is_beyond_maxenrollment!!!";
+SET output = 0;
+ELSEIF NOT EXISTS(
+SELECT L.UoSCode
+from lecture L
+where L.UoSCode = CourseNum
+)
+THEN
+SET notif = "2";
+SET output = 2;
+
+END if;
+
+if (t_error=1) 
+then begin
+set notif='ERROR, rollback';
+rollback;
+end;
+else begin 
+commit;
+end;
+end if;
+ 
+
+END
---
+++ README.md
@@ -0,0 +1,29 @@
+# Student_Academic_Database_System
+The Database client of this Student Academic Database System is implemented by using C++ and MySQL, it is the second projects of EECS 495 Intro to Database System of Northwestern University, the original student database is provided by professor Peter Scheuermann in 2017.
+
+Here are the schema of the database:
+
+STUDENT (ID, NAME, PASSWORD, ADDRESS)
+
+FACULTY (ID, NAME, DEPTID, PASSWORD, ADDRESS)
+
+CLASSROOM (CLASSROOM_ID, SEATS, TYPE)
+
+UOSOFFERING (UOSCODE, SEMESTER, YEAR, TEXTBOOK, ENROLLMENT, MAXENROLLMENT, INSTRUCTOR_ID)
+
+UNITSOFSTUDY (UOSCODE, DEPT_ID, UOSNAME, CREDITS)
+
+WHENOFFERED (UOSCODE, SEMESTER)
+
+REQUIRES(UOSCODE, PREREQ_UOSCODE, ENFORCED_SINCE)
+
+TRANSCRIPT (STUDENT_ID, UOSCODE, SEMESTER, YEAR, GRADE)
+
+LECTURE (CLASSROOM_ID, UOSCODE, SEMESTER, YEAR, CLASS_TIME)
+
+# Functionalities
+The implemented screen flow of the database application
+![app]()
+
+# Requirement and Restriction 
+The requirement and the restriction can be viewed from the Project_des.pdf file.
